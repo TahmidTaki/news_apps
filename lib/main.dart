6eba2e8f0b2af501d27dc9194841a51ff0026a2e
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-02-04 21:50:36 +0800

    main dart edited

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthWrapper(),
+    );
+  }
+}
